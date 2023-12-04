6c9146dcf012fc74efd1428fbefeea36822ba31f
stoclet sylvain <cronosu@hotmail.fr>
2023-12-04 14:29:50 +0100

    test

--- Backend/app.js
+++ Backend/app.js
@@ -7,6 +7,13 @@ const swaggerUi = require('swagger-ui-express')
 const yaml = require('yamljs')
 const swaggerDocs = yaml.load('swagger.yaml')
 const app = express()
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
